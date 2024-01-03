bf6617217e4328ae6a752f2e9a222492a8cd4357
Rodrygo Larreategui <verovillavicencio9@gmail.com>
2024-01-02 22:41:12 -0600

    first commit

---
+++ GestionVehicular/appGestionVehicular/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppgestionvehicularConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'appGestionVehicular'
---
+++ GestionVehicular/appGestionVehicular/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def BASE(request):
+    return render(request,'base.html')
