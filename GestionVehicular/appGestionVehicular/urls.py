84465c74f74ee9e10412f2ff53cb89ef280711d7
Rodrygo Larreategui <verovillavicencio9@gmail.com>
2024-01-05 22:47:07 -0600

    Crear enlaces

--- GestionVehicular/appGestionVehicular/urls.py
+++ GestionVehicular/appGestionVehicular/urls.py
@@ -4,6 +4,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BASE,name="BASE"),
-    path('Hme', views.LOGIN,name="LOGIN"),
+    path('HME', views.BASE,name="BASE"),
+    path('', views.LOGIN,name="LOGIN"),
+    path('Personal ', views.PERSONAL,name="PERSONAL"),
+    path('Dependencias ', views.DEPENDENCIAS,name="DEPENDENCIAS"),
+    path('Mantenimiento ', views.MANTENIMIENTO,name="MANTENIMIENTO"),
+    path('Vehiculos ', views.VEHICULOS,name="VEHICULOS"),
+    path('Solicitud ', views.SOLICITUD,name="SOLICITUD"),
+    path('Ajustes ', views.AJUSTES,name="AJUSTES"),
 ]
--- GestionVehicular/appGestionVehicular/views.py
+++ GestionVehicular/appGestionVehicular/views.py
@@ -4,4 +4,22 @@ def BASE(request):
     return render(request,'base.html')
 
 def LOGIN(request):
-    return render(request,'login.html')
+    return render(request,'login.html') 
+
+def PERSONAL(request):
+    return render(request,'personal.html')
+
+def DEPENDENCIAS(request):
+    return render(request,'dependencias.html')
+
+def MANTENIMIENTO(request):
+    return render(request,'mantenimiento.html')
+
+def VEHICULOS(request):
+    return render(request,'vehiculos.html') 
+
+def SOLICITUD(request):
+    return render(request,'solicitud.html')   
+
+def AJUSTES(request):
+    return render(request,'ajustes.html')                                                                       
