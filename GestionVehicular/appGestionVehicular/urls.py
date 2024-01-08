9663a981451b439b0a6c5002b98e4b289464dee3
Rodrygo Larreategui <verovillavicencio9@gmail.com>
2024-01-07 22:50:39 -0600

    ACTUALIZACION

--- GestionVehicular/appGestionVehicular/urls.py
+++ GestionVehicular/appGestionVehicular/urls.py
@@ -6,10 +6,10 @@ from . import views
 urlpatterns = [
     path('HME', views.BASE,name="BASE"),
     path('', views.LOGIN,name="LOGIN"),
-    path('Personal ', views.PERSONAL,name="PERSONAL"),
-    path('Dependencias ', views.DEPENDENCIAS,name="DEPENDENCIAS"),
-    path('Mantenimiento ', views.MANTENIMIENTO,name="MANTENIMIENTO"),
-    path('Vehiculos ', views.VEHICULOS,name="VEHICULOS"),
-    path('Solicitud ', views.SOLICITUD,name="SOLICITUD"),
-    path('Ajustes ', views.AJUSTES,name="AJUSTES"),
+    path('Personal', views.PERSONAL,name="PERSONAL"),
+    path('Dependencias', views.DEPENDENCIAS,name="DEPENDENCIAS"),
+    path('Mantenimiento', views.MANTENIMIENTO,name="MANTENIMIENTO"),
+    path('Vehiculos', views.VEHICULOS,name="VEHICULOS"),
+    path('Solicitud', views.SOLICITUD,name="SOLICITUD"),
+    path('Ajustes', views.AJUSTES,name="AJUSTES"),
 ]
